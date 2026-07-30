75ea22d6b3e7f4b3308c2d48c9821ec412693a28
Silas Teng <silasxciv@gmail.com>
2026-07-30 23:28:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import { Waline } from '@waline/server';
+import Waline from '@waline/vercel';
 
 Waline({
-  // 显式指定使用 Postgres 引擎
   type: 'postgres',
-  
-  // 传入 Supabase 数据库连接配置
   db: {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT || 5432,
@@ -12,7 +9,6 @@ Waline({
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE || 'postgres',
   },
-  
   jwtSecret: process.env.JWT_SECRET || 'waline_secret_123456',
   port: process.env.PORT || 8360,
 });
