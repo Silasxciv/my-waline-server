84150536dbba041442b2d6499d57b5b6f8833374
Silas Teng <silasxciv@gmail.com>
2026-07-30 22:59:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import Waline from '@waline/vercel';
 
+// 强制设置环境变量，确保 Waline 底层能感知 DB 配置
+process.env.DB_TYPE = 'postgres';
+
 Waline({
   type: 'postgres',
-  db: process.env.PG_DB_URL,
+  pgDbUrl: process.env.PG_DB_URL,
+  jwtSecret: process.env.JWT_SECRET || 'waline_secret_key',
   port: process.env.PORT || 8360,
 });
