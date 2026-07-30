5cce9140fbcd1ea8280c4138fe0877043aa84939
Silas Teng <silasxciv@gmail.com>
2026-07-30 23:04:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import { Waline } from '@waline/server';
+import Waline from '@waline/vercel';
 
 Waline({
-  type: 'postgres',
-  db: process.env.PG_DB_URL,
-  jwtSecret: process.env.JWT_SECRET || 'waline_secret_key',
   port: process.env.PORT || 8360,
 });
